e75178db75445f97f80334a0eb61b41d1376bc12
DennisWolfgang <31799620+DennisWolfgang@users.noreply.github.com>
2020-07-31 01:11:45 -0400

    Create spanish.lua

---
+++ ttt2_explosive_crosstol/lua/lang/spanish/spanish.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "spanish" )
+L[ "ttt2_explosive_crosstol_name" ] = "Pistolallesta Explosiva"
+L[ "ttt2_explosive_crosstol_desc" ] = "Pistola que dispara flechas explosivas."
+L[ "ttt2_explosive_crosstol_help1" ] = "{primaryfire} Dispara e impacta"
+L[ "ttt2_explosive_crosstol_help2" ] = "{secondaryfire} 30 segundos de música relajante"
